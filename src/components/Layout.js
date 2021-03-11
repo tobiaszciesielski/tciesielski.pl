41a529e2f2eb01c38c0f6447b540848d5066e993
Tobiasz Ciesielski <t.ciesielski1177@gmail.com>
2021-03-11 23:09:17 +0100

    Created global style

---
+++ src/components/Layout.js
@@ -0,0 +1,20 @@
+import React from "react"
+import { createGlobalStyle } from "styled-components"
+
+const GlobalStyle = createGlobalStyle`
+  body, html {
+    margin: 0;
+    padding: 0;
+    min-height: 100%;
+    background-color: #222;
+  }
+`
+
+const Layout = ({ children }) => {
+  return <React.Fragment>
+      <GlobalStyle/>
+      {children}
+  </React.Fragment>
+}
+
+export default Layout;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,19 @@
 import React from 'react'
+import Layout from './../components/Layout';
 import styled from "styled-components"
 
 const Title = styled.h1`
-  color: #000;
+  color: #FFF;
+  text-align: center;
   &::first-letter {
     color: #00FFA3;
   }
 `
 
 const IndexPage = () => {
-  return <Title>Tobiasz Ciesielski</Title>
+  return <Layout>
+    <Title>Tobiasz Ciesielski</Title>
+  </Layout>
 }
 
 export default IndexPage
