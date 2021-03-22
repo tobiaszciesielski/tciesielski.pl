7f541b09d53f6fea2b7aec23375f8d902166d099
Tobiasz Ciesielski <t.ciesielski1177@gmail.com>
2021-03-22 22:16:00 +0100

    Fetching data from sitemeta data

--- src/components/SubTitle.js
+++ src/components/SubTitle.js
@@ -1,34 +1,43 @@
 import React from 'react';
 import styled from 'styled-components';
 import Typewriter from 'typewriter-effect';
+import { graphql, useStaticQuery } from 'gatsby';
 
 const StyledSubTitle = styled.div`
   text-align: left;
   font-size: 18px;
   margin-top: 15px;
-  margin-left: 30px;
-  width: 100%;
 
   .Typewriter__cursor {
     color: ${({ theme }) => theme.colors.touch};
     font-size: 22px;
   }
 
-  @media ${({ theme: { media } }) => media.mobile} {
-    margin-left: 60px;
-  }
+  margin-left: 40px;
 `;
 
-const SubTitle = () => (
-  <StyledSubTitle>
-    <Typewriter
-      options={{
-        strings: ['front-end developer', 'blogger', 'designer'],
-        autoStart: true,
-        loop: true,
-      }}
-    />
-  </StyledSubTitle>
-);
+const SubTitle = () => {
+  const data = useStaticQuery(graphql`
+    {
+      site {
+        siteMetadata {
+          subTitle
+        }
+      }
+    }
+  `);
+
+  return (
+    <StyledSubTitle>
+      <Typewriter
+        options={{
+          strings: data.site.siteMetadata.subTitle,
+          autoStart: true,
+          loop: true,
+        }}
+      />
+    </StyledSubTitle>
+  );
+};
 
 export default SubTitle;
