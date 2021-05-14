a7370d3027c746ab1df90f8d1792577b572a6496
Tobiasz Ciesielski <t.ciesielski1177@gmail.com>
2021-05-14 12:28:49 +0200

    Updated gitignore

--- src/components/atoms/Menu.js
+++ src/components/atoms/Menu.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Link } from 'gatsby';
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
 import SmoothScrollAnchor from './SmoothScrollAnchor';
